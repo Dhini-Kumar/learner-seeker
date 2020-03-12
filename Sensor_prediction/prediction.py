2346fa1f6bb3ca40c91a52a532d8040516290c92
Dhini-Kumar <61245908+Dhini-Kumar@users.noreply.github.com>
2020-03-12 19:12:45 +0530

    Add files via upload
    Modified

--- Sensor_prediction/prediction.py
+++ Sensor_prediction/prediction.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import pickle
+import os, random
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
 loaded_model = pickle.load(open('trained_model/eagle_model.yml', 'rb'))
 
 # New data for prediction
+
 new_dataset = pd.read_csv('Test_data/Lose_test.csv', header = None)
 Trans_data = new_dataset.T
 to_predict = Trans_data.iloc[:,:].values
@@ -14,6 +16,6 @@ to_predict = Trans_data.iloc[:,:].values
 y_pred = loaded_model.predict(to_predict)
 
 if y_pred == 1:
-	print ("The Nut is Tight : ",y_pred )
+	print ("Nut is tight for ",os.path.basename("Test_data/Lose_test.csv"))
 else:
-	print ("Nut is lose : ",y_pred) 
+	print ("Nut is lose for ",os.path.basename("Test_data/Lose_test.csv"))
